5fea7b797f93f0c30636c0f7bd89b83fbdb07321
unknown <munesh.luvinika@gmail.com>
2022-05-28 00:31:00 -0400

    HIS-301 Related changes

--- src/main/java/in/ashokit/Demo.java
+++ src/main/java/in/ashokit/Demo.java
@@ -2,9 +2,11 @@ package in.ashokit;
 
 
 public class Demo extends RuntimeException{
-
+private Map<String,Object>cache=new HashMap<String,Object>();
 int a=10;
 public void doProcess(){
 }
-
+public void loadDataTCache(){
+//logic to load
+}
 }
